fb679b53cba74c1fe7e73b3573fc3d40e9dd052a
Bogdan Nicolae <61329679+Nicbogdan@users.noreply.github.com>
2024-02-17 22:43:09 +0100

    added files
    added html form file and php file that generates rdp connection

---
+++ rdp2.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: application/rdp');
+header('Content-Disposition: attachment; filename="conn.rdp"');
+echo('full address:s:'.preg_replace('/[^a-z^0-9^.^:^]/', '',$_POST['address']));
+?>
