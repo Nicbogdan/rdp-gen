e46c20c4d43e4469176a3510e9fd74a4ab08b575
Bogdan Nicolae <61329679+Nicbogdan@users.noreply.github.com>
2024-02-17 23:39:11 +0100

    Update rdp2.php
    added comment to the file

--- rdp2.php
+++ rdp2.php
@@ -1,4 +1,5 @@
 <?php
+// rdp-gen github repository 
 header('Content-Type: application/rdp');
 header('Content-Disposition: attachment; filename="conn.rdp"');
 echo('full address:s:'.preg_replace('/[^a-z^0-9^.^:^]/', '',$_POST['address']));
